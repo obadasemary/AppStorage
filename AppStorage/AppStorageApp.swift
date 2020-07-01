4360e5269fd254d197e731865a7de7941f6f926c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-01 15:38:10 +0300

    Initial Commit

---
+++ AppStorage/AppStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageApp.swift
+//  AppStorage
+//
+//  Created by Abdelrahman Mohamed on 1.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
